83c3a7868d78758e87b48645b7215a34bcdab314
maxime.dezette <m.dezette@gmail.com>
2024-01-06 12:32:14 +0100

    Initial commit :tada:

---
+++ kata-c/TestShould.cs
@@ -0,0 +1,14 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace kata_c;
+
+[TestFixture]
+public class TestShould
+{
+    [Test]
+    public void Indicate_that_true_is_not_false()
+    {
+        Check.That(true).IsFalse();
+    }
+}
